ba9a2037c2015ed236a4a93afe210d5f2f48f02f
Bruno <brunomartins1030@gmail.com>
2022-08-28 14:37:01 -0300

    Refactoring code to remove hard coded

--- kafka-consumer-database/src/main/java/com/bruno/springboot/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/com/bruno/springboot/KafkaDatabaseConsumer.java
@@ -4,6 +4,7 @@ import com.bruno.springboot.entity.WikimediaData;
 import com.bruno.springboot.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class KafkaDatabaseConsumer {
         this.dataRepository = dataRepository;
     }
 
-    @KafkaListener(topics = "wikimedia_recentchange", groupId = "myGroup")
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(String eventMessage){
         LOGGER.info(String.format("Event message received -> %s", eventMessage));
 
